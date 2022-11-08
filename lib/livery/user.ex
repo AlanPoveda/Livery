92ded36e8138dfbdd784de3c3ca19c3fafd42ecf
Alan Poveda <alsepoveda@gmail.com>
2022-11-07 21:08:34 -0300

    Create first user changeset

--- lib/livery/user.ex
+++ lib/livery/user.ex
@@ -1,13 +1,15 @@
 defmodule LiveryWeb.User do
   use Ecto.Schema
-  import Repo.Changeset
+  import Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: :true}
 
+  @required_params [:cpf, :name, :cpf, :email, :password]
   schema "users" do
-    field :age, :interger
+    field :age, :integer
     field :name, :string
     field :cpf, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :email, :string
     field :address, :string
@@ -16,7 +18,19 @@ defmodule LiveryWeb.User do
     timestamps()
   end
 
-
-  def changeset() do
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params,@required_params)
+    |> validate_required(@required_params)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do  
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+
 end
