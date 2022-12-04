78f7ee6f2d88ad4a3258227f5c3de0b625571eb9
Alan Poveda <alsepoveda@gmail.com>
2022-12-04 15:37:30 -0300

    FallbackController, config multiple errors

--- lib/livery_web/views/error_view.ex
+++ lib/livery_web/views/error_view.ex
@@ -11,21 +11,22 @@ defmodule LiveryWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
+	def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
-  def render("400.json", {:error, result: %Ecto.Changeset{} = changeset}) do
+  def render("error.json", %{result: %Ecto.Changeset{} = changeset}) do
     %{
-      message: translate_error(changeset)
+      message: translate_errors(changeset)
     }
   end
 
-  defp tranlate_error(changeset) do
+  defp translate_errors(changeset) do
     traverse_errors(changeset, fn {msg, opts} ->
       Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
-          opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
-          end)
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
     end)
   end
+  
 end
